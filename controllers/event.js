e68803eb5877897419eed804b1acf37ca3d54f8e
Lukas Vollmer <github@lukasvollmer.com>
2018-01-22 10:29:15 +0100

    es lint config

--- controllers/event.js
+++ controllers/event.js
@@ -1,26 +1,27 @@
-'use strict';
+'use strict'
 
-const errors = require('../util/error_handling');
+const errors = require('../util/error_handling')
 
 function show (req, res) {
-    global.datastore.get(global.datastore.key(['Event', parseInt(req.params.eventId)]))
+  global.datastore.get(global.datastore.key(['Event', parseInt(req.params.eventId)]))
         .then((results) => {
-            res.json({"status": "OK", "event":
-                    {
-                        "name": results[0].name,
-                        "description": results[0].description,
-                        "startingDate": results[0].startingDate,
-                        "endingDate": results[0].endingDate,
-                        "location": results[0].location,
-                        "coordinates": results[0].coordinates,
-                        "primaryColor": results[0].primaryColor,
-                        "accentColor": results[0].accentColor
-                    }});
+          res.json({'status': 'OK',
+            'event':
+            {
+              'name': results[0].name,
+              'description': results[0].description,
+              'startingDate': results[0].startingDate,
+              'endingDate': results[0].endingDate,
+              'location': results[0].location,
+              'coordinates': results[0].coordinates,
+              'primaryColor': results[0].primaryColor,
+              'accentColor': results[0].accentColor
+            }})
         }).catch(function (err) {
-            errors.handle(err, res);
-    })
+          errors.handle(err, res)
+        })
 }
 
 module.exports = {
-    show: show
-};
+  show: show
+}
--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-'use strict';
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 3000;
-var bodyParser = require('body-parser');
-var routes = require('./routes/index');
+'use strict'
+var express = require('express')
+var app = express()
+var port = process.env.PORT || 3000
+var bodyParser = require('body-parser')
+var routes = require('./routes/index')
 
-GLOBAL.datastore = require('@google-cloud/datastore')({
+global.datastore = require('@google-cloud/datastore')({
   projectId: 'conferencify-2018',
   keyFilename: 'keyfile.json'
 });
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
-app.use("/api/v1", routes);
+app.use('/api/v1', routes)
 
 app.listen(port);
 
-console.log('Shivt RESTful API server started on: ' + port);
+console.log('Shivt RESTful API server started on: ' + port)
