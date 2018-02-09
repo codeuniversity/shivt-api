0fc394bb0d56fa3c2f8565fcec730302dda2034e
Lukas Vollmer <github@lukasvollmer.com>
2018-02-09 15:00:43 +0100

    change global structure, add global config, editor config, route validation

--- controllers/speakers.js
+++ controllers/speakers.js
@@ -3,36 +3,31 @@
 const errors = require('../util/error_handling')
 const helpers = require('../util/helpers')
 
-function getSpeakers(eventId, callback) {
-    const query = global.datastore.createQuery('Attendee').filter('type', '=', 1).hasAncestor(datastore.key(['Event', parseInt(eventId)]))
-    global.datastore.runQuery(query, (err, entities) => {
-        let speakers = []
-        entities.forEach(function (speaker) {
-            speakers.push(helpers.speakerSerializer(speaker))
-            if (speakers.length === entities.length) {
-                helpers.sortByKey(speakers,'lastname')
-                callback(speakers)
-                //res.json({'status': 'OK', 'speakers': speakers})
-            }
-        })
+function getSpeakers (eventId, callback) {
+  const query = global.datastore.createQuery('Attendee').filter('type', '=', 1).hasAncestor(datastore.key(['Event', parseInt(eventId)]))
+  global.datastore.runQuery(query, (err, entities) => {
+    let speakers = []
+    entities.forEach(function (speaker) {
+      speakers.push(helpers.speakerSerializer(speaker))
+      if (speakers.length === entities.length) {
+        helpers.sortByKey(speakers, 'lastname')
+        callback(speakers)
+        //res.json({'status': 'OK', 'speakers': speakers})
+      }
     })
+  })
 }
 
-
-function show(req, res) {
-
-    // IMPORTANT: In case the requested ID has an ancestor, datastore.key takes four parameters.
-
-    const speakerKey = global.datastore.key(['Event', parseInt(req.params.eventId), 'Attendee', parseInt(req.params.speakerId)])
-    global.datastore.get(speakerKey, function (err, speaker) {
-        if(err) {
-            errors.handle(err, res)
-        }
-        res.json({'status': 'OK', 'speaker': helpers.speakerSerializer(speaker)})
-    })
+function show (req, res) {
+  // IMPORTANT: In case the requested ID has an ancestor, datastore.key takes four parameters.
+  const speakerKey = global.datastore.key(['Event', parseInt(req.params.eventId), 'Attendee', parseInt(req.params.speakerId)])
+  global.datastore.get(speakerKey, function (err, speaker) {
+    errors.handle(err, res)
+    res.json({'status': 'OK', 'speaker': helpers.speakerSerializer(speaker)})
+  })
 }
 
 module.exports = {
-    getSpeakers: getSpeakers,
-    show: show
-};
+  getSpeakers: getSpeakers,
+  show: show
+}
--- util/helpers.js
+++ util/helpers.js
@@ -1,65 +1,66 @@
-function normalizeDate(date) {
-    date.setUTCHours(0);
-    date.setUTCSeconds(0);
-    date.setUTCMilliseconds(0);
-    return date;
+function normalizeDate (date) {
+  date.setUTCHours(0)
+  date.setUTCSeconds(0)
+  date.setUTCMilliseconds(0)
+  return date
 }
 
-function sortTalk(talks, talk) {
-    let found = false;
-    for(let i = 0; i < talks.length; i++) {
-        if(normalizeDate(new Date(talk.startingDate)).toISOString() === talks[i].date) {
-            talks[i].talks.push(talk)
-            found = true;
-            break;
-        }
+function sortTalk (talks, talk) {
+  let found = false
+  for (let i = 0; i < talks.length; i++) {
+    if (normalizeDate(new Date(talk.startingDate)).toISOString() === talks[i].date) {
+      talks[i].talks.push(talk)
+      found = true
+      break
     }
-    if(!found) {
-        talks.push({
-            'date': normalizeDate(new Date(talk.startingDate)).toISOString(),
-            'talks': [
-                talk
-            ]
-        })
-    }
+  }
+  if (!found) {
+    talks.push({
+      'date': normalizeDate(new Date(talk.startingDate)).toISOString(),
+      'talks': [
+        talk
+      ]
+    })
+  }
 }
 
-function sortByKey(array, key) {
-    return array.sort(function(a, b) {
-        var x = a[key]; var y = b[key];
-        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
-    });
+function sortByKey (array, key) {
+  return array.sort(function (a, b) {
+    var x = a[key]
+    var y = b[key]
+    return ((x < y) ? -1 : ((x > y) ? 1 : 0))
+  })
 }
 
-function talkSerializer(talk, speakers) {
-    return {
-        'id': talk[global.datastore.KEY].id,
-        'name': talk.name,
-        'description': talk.description,
-        'type': talk.type,
-        'startingDate': talk.startingDate,
-        'endingDate': talk.endingDate,
-        'location': talk.location,
-        'speakers': speakers
-    }
+function talkSerializer (talk, speakers) {
+  return {
+    'id': talk[global.datastore.KEY].id,
+    'name': talk.name,
+    'description': talk.description,
+    'type': talk.type,
+    'startingDate': talk.startingDate,
+    'endingDate': talk.endingDate,
+    'location': talk.location,
+    'speakers': speakers
+  }
 }
 
-function speakerSerializer(speaker) {
-    return {
-        'id': speaker[global.datastore.KEY].id,
-        'firstname': speaker.firstname,
-        'lastname': speaker.lastname,
-        'photo': speaker.photo,
-        'company': speaker.company,
-        'position': speaker.position,
-        'description': speaker.description,
-        'socialMedia': {
-        'linkedIn': speaker.linkedIn
+function speakerSerializer (speaker) {
+  return {
+    'id': speaker[global.datastore.KEY].id,
+    'firstname': speaker.firstname,
+    'lastname': speaker.lastname,
+    'photo': speaker.photo,
+    'company': speaker.company,
+    'position': speaker.position,
+    'description': speaker.description,
+    'socialMedia': {
+      'linkedIn': speaker.linkedIn
     }
-    }
+  }
 }
 
-function sponsorRankSerializer(sponsorRank) {
+function sponsorRankSerializer (sponsorRank) {
   return {
     'id': sponsorRank[global.datastore.KEY].id,
     'name': sponsorRank.name,
@@ -67,7 +68,7 @@ function sponsorRankSerializer(sponsorRank) {
   }
 }
 
-function sponsorSerializer(sponsor) {
+function sponsorSerializer (sponsor) {
   return {
     'id': sponsor[global.datastore.KEY].id,
     'name': sponsor.name,
@@ -77,22 +78,20 @@ function sponsorSerializer(sponsor) {
   }
 }
 
-function userSerializer(user) {
+function userSerializer (user) {
   return {
     'id': user[0][global.datastore.KEY].id,
     'type': user[0].type
   }
 }
 
-
-
 module.exports = {
-    normalizeDate: normalizeDate,
-    sortTalk: sortTalk,
-    sortByKey: sortByKey,
-    talkSerializer: talkSerializer,
-    speakerSerializer: speakerSerializer,
-    sponsorRankSerializer: sponsorRankSerializer,
-    sponsorSerializer: sponsorSerializer,
-    userSerializer: userSerializer
+  normalizeDate: normalizeDate,
+  sortTalk: sortTalk,
+  sortByKey: sortByKey,
+  talkSerializer: talkSerializer,
+  speakerSerializer: speakerSerializer,
+  sponsorRankSerializer: sponsorRankSerializer,
+  sponsorSerializer: sponsorSerializer,
+  userSerializer: userSerializer
 }
