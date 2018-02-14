68173a3de1fcf5dab1ee86295ddd224359fbb00d
Lukas Vollmer <github@lukasvollmer.com>
2018-02-14 10:32:18 +0100

    add event create

--- routes/privatRoutes.js
+++ routes/privatRoutes.js
@@ -1,9 +1,12 @@
-'use strict';
+'use strict'
 
-const express = require( 'express' );
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-const testRoutes = require( './privat/test' );
-router.use( '/test', testRoutes );
+const testRoutes = require('./privat/test')
+const eventRoutes = require('./privat/events')
 
-module.exports = router;
+router.use('/test', testRoutes)
+router.use('/events', eventRoutes)
+
+module.exports = router
